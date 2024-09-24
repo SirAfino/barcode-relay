a30c00e7aeb2f471af3de2e0d5451fef5ce5f65a
Gabriele Serafino <serafino.9966@gmail.com>
2024-09-24 06:48:56 +0200

    Added redis stream sender class

---
+++ redis_stream_sender.py
@@ -0,0 +1,65 @@
+# 
+# This file is part of the BarcodeRelay distribution (https://github.com/SirAfino/barcode-relay).
+# Copyright (c) 2024 Gabriele Serafino.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from queue import Queue
+from sender import Sender
+from redis import Redis
+from time import sleep
+
+class RedisStreamSender(Sender):
+    _redis: Redis
+    _stream_name: str
+
+    def __init__(
+        self,
+        relay_name: str, 
+        queue: Queue,
+        redis_host: str,
+        redis_port: int,
+        redis_username: str,
+        redis_password: str,
+        redis_stream: str,
+        polling_ms: int = 1000
+    ):
+        super().__init__(relay_name, queue, polling_ms)
+        self._redis = Redis(
+            host=redis_host,
+            port=redis_port,
+            username=redis_username,
+            password=redis_password,
+            decode_responses=True
+        )
+        self._stream_name = redis_stream
+
+    def _send(self, code, ts):
+        sent = False
+        data = { 'relay': self._relay_name, 'code': code, 'ts': ts }
+
+        while self._run and not sent:
+            try:
+                self._redis.xadd(self._stream_name, data)
+                sent = True
+            except Exception as e:
+                seconds = 5
+                self._logger.info(f"Error while sending message, retry in {seconds}s...")
+                self._logger.info(e)
+                sleep(seconds)
+
+    def stop(self):
+        super().stop()
+        if self._redis:
+            self._redis.close()
--- sender.py
+++ sender.py
@@ -18,19 +18,23 @@
 from threading import Thread
 from queue import Queue, Empty
 from logging import Logger, getLogger
+import json
 
 class Sender:
     _logger: Logger
     _run: bool
     _thread: Thread
+
+    _relay_name: str
     _queue: Queue = None
     _polling_ms: int
 
-    def __init__(self, queue: Queue, polling_ms: int = 1000):
+    def __init__(self, relay_name: str, queue: Queue, polling_ms: int = 1000):
         self._logger = getLogger()
         self._run = False
         self._thread = None
 
+        self._relay_name = relay_name
         self._queue = queue
         self._polling_ms = polling_ms
 
@@ -44,7 +48,7 @@ class Sender:
         while self._run:
             try:
                 (ts, code) = self._queue.get(True, self._polling_ms / 1000.0)
-                self._logger.info(f"Sending scan {code}")
+                self._logger.info(f"Sending scan {json.dumps({'code': code})}")
                 self._send(code, ts)
             except Empty:
                 pass
