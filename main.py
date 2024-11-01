fc035417d67e0bdd588053e158279e4b18dc90e9
Gabriele Serafino <serafino.9966@gmail.com>
2024-11-01 21:38:52 +0100

    Import interception dinamically to allow correct start on linux systems

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-# 
+#
 # This file is part of the BarcodeRelay distribution (https://github.com/SirAfino/barcode-relay).
 # Copyright (c) 2024 Gabriele Serafino.
-# 
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License 
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from queue import Queue
-from sys import exit
-from _version import __version__
 import logging
 import argparse
-from device_config import DeviceConfig
-from interception_multidevice_reader import InterceptionMultiDeviceReader
-import interception_util
-import yaml
-from interception_device_reader import InterceptionDeviceReader
 from time import sleep
-from redis_stream_sender import RedisStreamSender
+from queue import Queue
+import sys
+import json
+from datetime import datetime
+import os
+import yaml
+from _version import __version__
+from device_config import DeviceConfig
+import interception_util
 from sender import Sender
 
 CONFIG_FILEPATH = "config.yml"
 LOGS_FILEPATH = "app.log"
 
-def licenseNotice() -> str:
+def license_notice() -> str:
+    """Print license notice text"""
     return (
         "BarcodeRelay  Copyright (C) 2024  Gabriele Serafino\n"+
         "This program comes with ABSOLUTELY NO WARRANTY.\n"+
@@ -40,67 +41,78 @@ def licenseNotice() -> str:
         "under certain conditions."
     )
 
-def loadConfiguration(filepath: str = CONFIG_FILEPATH):
+def load_configuration(filepath: str = CONFIG_FILEPATH):
+    """Load working configuration from specified file"""
     try:
-        with open(filepath, 'r') as file:
+        with open(filepath, 'r', encoding="utf-8") as file:
             configuration = yaml.safe_load(file)
         return configuration
     except FileNotFoundError:
         return None
 
-def setupLogger(level = logging.INFO, filepath: str = None):
+def setup_logger(level = logging.INFO, filepath: str = None):
+    """Setup logger for console and file logging"""
     logger = logging.getLogger()
     logger.setLevel(level)
-    
+
     formatter = logging.Formatter(
         '%(asctime)s %(levelname)s [%(component)s] [%(uuid)s] %(message)s',
         defaults={'component': 'APP', 'uuid': ''}
     )
 
-    streamHandler = logging.StreamHandler()
-    streamHandler.setLevel(level)
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
     if filepath is not None:
-        fileHandler = logging.FileHandler(filepath)
-        fileHandler.setLevel(level)
-        fileHandler.setFormatter(formatter)
-        logger.addHandler(fileHandler)
+        file_handler = logging.FileHandler(filepath)
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     return logger
 
 
 def main():
-    print(licenseNotice())
+    """Main function"""
+    print(license_notice())
     print()
 
     # Parse command line arguments to check if the user just wants to list USB devices, otherwise
     # start the program normally
-    argsParser = argparse.ArgumentParser()
-    argsParser.add_argument("-l", "--list", action="store_true", help="List available USB devices HWID")
-    args = argsParser.parse_args()
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument(
+        "-l", "--list", action="store_true", help="List available USB devices HWID")
+    args_parser.add_argument("-t", "--test", help="Test send a single code")
+    args = args_parser.parse_args()
 
     if args.list:
         print("List of attached HID USB devices (Hardware ID):")
-        devices = interception_util.listKeyboardDevices()
+        if os.name == "nt":
+            devices = interception_util.list_keyboard_devices()
+        else:
+            # TODO: implement for linux
+            devices = []
+            
         for device in devices:
             print(f" - {device}")
 
         if len(devices) == 0:
             print("   No device found")
-            
+
         print()
-        exit(0)
+        sys.exit(0)
 
-    logger = setupLogger(logging.DEBUG, LOGS_FILEPATH)
-    logger.info(f'BarcodeRelay - v{__version__}')
+    logger = setup_logger(logging.DEBUG, LOGS_FILEPATH)
+    logger.info('BarcodeRelay - v%s', __version__)
 
-    config = loadConfiguration()
+    config = load_configuration()
     if config is None:
-        logger.error(f'Error while loading configuration file from "{CONFIG_FILEPATH}".')
-        logger.error(f'Make sure the configuration file exists and conforms the provided configuration template.')
-        exit(-1)
+        logger.error('Error while loading configuration file from "%s".', CONFIG_FILEPATH)
+        logger.error('Make sure the configuration file exists \
+                     and conforms the provided configuration template.')
+        sys.exit(-1)
 
     logger.info("Configuration loaded")
 
@@ -114,9 +126,10 @@ def main():
             c['full_scan_regex']
         ))
 
-    device_reader = InterceptionMultiDeviceReader(device_configs, queue)
-
     if config['target']['type'] == 'redis_stream':
+        #pylint: disable=import-outside-toplevel
+        from redis_stream_sender import RedisStreamSender
+        #pylint: enable=import-outside-toplevel
         sender = RedisStreamSender(
             config['id'],
             queue,
@@ -131,6 +144,27 @@ def main():
             config['id'],
             queue
         )
+
+    if args.test:
+        sender.start()
+        ts = int(datetime.now().timestamp())
+        queue.put((config['devices'][0]['id'], args.test, ts))
+        logger.info(
+            "Simulate scan: %s", json.dumps({'code': args.test}),
+            extra={ 'component': f"READER:{config['devices'][0]['id']}" }
+        )
+        sleep(1)
+        sender.stop()
+        sys.exit(0)
+
+    #pylint: disable=import-outside-toplevel
+    if os.name == 'nt':
+        from interception_multidevice_reader import InterceptionMultiDeviceReader
+        device_reader = InterceptionMultiDeviceReader(device_configs, queue)
+    else:
+        from multidevice_reader import MultiDeviceReader
+        device_reader = MultiDeviceReader(device_configs, queue)
+    #pylint: enable=import-outside-toplevel
 
     device_reader.start()
     sender.start()
