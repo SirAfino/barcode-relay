91d1986f32e08ccf5899652a03e2a2f50316bfaa
Gabriele Serafino <serafino.9966@gmail.com>
2024-11-01 21:38:20 +0100

    Moved common interception functions to util file

--- interception_device_reader.py
+++ interception_device_reader.py
@@ -1,85 +1,66 @@
-# 
+#
 # This file is part of the BarcodeRelay distribution (https://github.com/SirAfino/barcode-relay).
 # Copyright (c) 2024 Gabriele Serafino.
-# 
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License 
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
 from datetime import datetime
 import json
-from device_reader import DeviceReader
-import interception_py.interception as interception
 import re
-from keycodes import codeToChar
+from device_reader import DeviceReader
+from interception_py import interception
+from interception_util import get_device_handle, regex_device_filter
+from keycodes import code_to_char
 
 class InterceptionDeviceReader(DeviceReader):
-    # Gets the device handle (integer) for the given HWID regex, return -1 if not found
-    # This function is needed because, using the default interception class to get the handle
-    # results in some issues that do not allow to get data from the device afterwards
-    def get_device_handle(self):
-        for i in range(interception.MAX_DEVICES):
-            device = interception.device(
-                interception.k32.CreateFileA(b'\\\\.\\interception%02d' % i, 0x80000000,0,0,3,0,0),
-                interception.k32.CreateEventA(0, 1, 0, 0),
-                interception.interception.is_keyboard(i)
-            )
-            hwid = device.get_HWID().decode("utf-16")
-            if re.match(self._config.hwid_regex, hwid) is not None:
-                return i
-
-        return -1
-    
-    def device_filter(self, interception: interception.interception):
-        def filter(device):
-            return 1 if re.match(self._config.hwid_regex, interception.get_HWID(device)) else 0
-        
-        return filter
+    """Device reader using the interception driver (Windows only)"""
 
     def run(self):
         # Instantiate the interception object
         c = interception.interception()
-        
+
         # Track the current device handle, needed to check if the device was
         # disconnected and reconnected again
         handle = None
-        
+
         # Keep a buffer of the device data
         buffer = ""
 
         while self._run:
             # Get the current device handle and check if its different from
             # the one we had on the previous cycle
-            new_handle = self.get_device_handle()
+            new_handle = get_device_handle(self._config.hwid_regex)
             if handle is None or new_handle != handle:
                 handle = new_handle
 
                 if handle == -1:
                     self._logger.info(
-                        f"Device disconnected",
+                        "Device disconnected",
                         extra={ 'component': 'READER' }
                     )
                 else:
                     self._logger.info(
-                        f"Device re/connected",
+                        "Device re/connected",
                         extra={ 'component': 'READER' }
                     )
-                
+
                     # Add a filter to capture data from the new device
                     c.set_filter(
-                        self.device_filter(c),
+                        regex_device_filter(c, [self._config.hwid_regex]),
                         interception.interception_filter_key_state.INTERCEPTION_FILTER_KEY_ALL.value
                     )
-            
+
             # Try to get data from the intercepted device
             device = c.wait(self._polling_ms)
             if device < 0:
@@ -87,22 +68,22 @@ class InterceptionDeviceReader(DeviceReader):
                 continue
 
             stroke = c.receive(device)
-            
+
             # Every event is intercepted from the target device, only interesting
             # one is the KEY_DOWN event
-            if type(stroke) is not interception.key_stroke:
+            if not isinstance(stroke, interception.key_stroke):
                 continue
             if stroke.state != interception.interception_key_state.INTERCEPTION_KEY_DOWN.value:
                 continue
-            
-            buffer += codeToChar(stroke.code)
+
+            buffer += code_to_char(stroke.code)
 
             # Check if the string is a full scan
             if re.match(self._config.full_scan_regex, buffer):
                 ts = int(datetime.now().timestamp())
                 self._queue.put((self._config.id, buffer, ts))
                 self._logger.info(
-                    f"Read scan: {json.dumps({'code': buffer})}",
+                    "Read scan: %s", json.dumps({'code': buffer}),
                     extra={ 'component': f"READER:{self._config.id}" }
                 )
                 buffer = ""
--- interception_util.py
+++ interception_util.py
@@ -1,26 +1,29 @@
-# 
+#
 # This file is part of the BarcodeRelay distribution (https://github.com/SirAfino/barcode-relay).
 # Copyright (c) 2024 Gabriele Serafino.
-# 
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License 
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from ctypes import *
+import re
+from ctypes import windll
 from interception_py import interception
 
-k32 = windll.LoadLibrary('kernel32')
+def list_keyboard_devices() -> list[str]:
+    """Returns a list of attached USB keyboard devices"""
 
-def listKeyboardDevices() -> list[str]:
+    k32 = windll.LoadLibrary('kernel32')
+
     hwids = []
 
     for i in range(interception.MAX_DEVICES):
@@ -35,8 +38,33 @@ def listKeyboardDevices() -> list[str]:
             is_keyboard
         )
         hwid = device.get_HWID().decode("utf-16")
-        
+
         if len(hwid) > 0:
             hwids.append(hwid)
 
     return hwids
+
+def regex_device_filter(_interception: interception.interception, regexs: list[str]):
+    """Returns a filter function that filters by multiple hwid regular expressions"""
+    def _filter(device):
+        for regex in regexs:
+            if re.match(regex, _interception.get_HWID(device)):
+                return 1
+
+        return 0
+
+    return _filter
+
+def get_device_handle(regex: str):
+    """Returns the device id for this USB device"""
+    for i in range(interception.MAX_DEVICES):
+        device = interception.device(
+            interception.k32.CreateFileA(b'\\\\.\\interception%02d' % i, 0x80000000,0,0,3,0,0),
+            interception.k32.CreateEventA(0, 1, 0, 0),
+            interception.interception.is_keyboard(i)
+        )
+        hwid = device.get_HWID().decode("utf-16")
+        if re.match(regex, hwid) is not None:
+            return i
+
+    return -1
